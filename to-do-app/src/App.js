2f89726a47795d04ac0c9cad89cb5e5770d8b67f
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-11-05 14:56:09 -0500

    added reducers and got the line through

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import ToDoForm from "./components/ToDoForm";
+import ToDoList from "./components/ToDoList.js";
+import { initialState, toDoReducer } from "./reducers/index.js";
+import "./App.css";
 
 function App() {
+  const [state, dispatch] = useReducer(toDoReducer, initialState);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={require("./imgs/todo.jpg")} />
+        <ToDoList dispatch={dispatch} data={state} />
+        <ToDoForm dispatch={dispatch} />
       </header>
     </div>
   );
--- to-do-app/src/components/ToDoList.js
+++ to-do-app/src/components/ToDoList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Todo from "./todo";
+import Todo from "./ToDo.js";
 
 const ToDoList = props => {
   return (
